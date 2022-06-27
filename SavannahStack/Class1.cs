47b9a8de680a1677819e1c77d1b82b759471e930
Stephen Odundo <steveremdi@gmail.com>
2022-06-27 22:15:46 +0300

    Added base functions

--- SavannahStack/Class1.cs
+++ SavannahStack/Class1.cs
@@ -2,6 +2,25 @@
 {
     public class Class1
     {
+        public void Push(string element)
+        {
 
+        }
+
+        // For multiple items
+        public void Push(string[] elements)
+        {
+
+        }
+
+        public string Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Peek()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
