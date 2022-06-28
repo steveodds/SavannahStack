02d1a2e858f8225508f41e3ccc3d8d2539c48ae5
Stephen Odundo <steveremdi@gmail.com>
2022-06-28 09:31:06 +0300

    Set up initialization details

--- SavannahStack/Stack_S.cs
+++ SavannahStack/Stack_S.cs
@@ -2,6 +2,20 @@
 {
     public class Stack_S
     {
+        static readonly int MAX_STACK_SIZE = 1000;
+        private int top;
+        private int[] stack = new int[MAX_STACK_SIZE];
+
+        public Stack_S()
+        {
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top < 0;
+        }
+
         public void Push(string element)
         {
 
